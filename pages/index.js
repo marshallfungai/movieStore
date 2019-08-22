4f59107e49fbc84d4c30738348947ee18f20babe
marshallfungai <fmarshall.m@gmail.com>
2019-08-22 17:53:27 +0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import {Button} from 'antd';
+import Link from 'next/link';
+
+//export default () => <div>Welcome to next.js! <Link href="/product">Product</Link></div>
+ReactDOM.render(<Button/>, mountNode);
